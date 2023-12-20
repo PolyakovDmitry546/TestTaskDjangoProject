92d9f97e6641d525eefd628302a2577f6367b5f3
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2023-12-20 21:24:36 +0500

    Add BuyView implementation to get stripe session

--- src/items/views.py
+++ src/items/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpRequest, JsonResponse
+from django.urls import reverse
+
 from django.views import View
 from django.views.generic import DetailView
 
 from items.models import Item
+from items.services import get_stripe_checkout_session
 
 
 class ItemView(DetailView):
@@ -12,4 +16,14 @@ class ItemView(DetailView):
 
 
 class BuyView(View):
-    pass
+    """Возвращает Stripe Session id для оплаты выбранного Item"""
+    def get(self, request: HttpRequest, id: int):
+        success_url = request.build_absolute_uri(reverse(
+            viewname='item-view',
+            args=[id,]
+        ))
+        session = get_stripe_checkout_session(
+            item_id=id,
+            success_url=success_url
+        )
+        return JsonResponse({"session_id": session.id})
