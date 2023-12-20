aaca0bb9a6a83f1b8447849dcfb1e030c410eac1
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2023-12-20 22:36:38 +0500

    Change IntegerField for Positive in price

--- src/items/models.py
+++ src/items/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()
 
     class Meta:
         db_table = "item"
